96d52d89a8401536ce61818a0fb50dd1542851c6
selfsame <joseph.parker.1@gmail.com>
2017-07-08 11:05:59 +0000

    repl view can send commands and walk through history, clear command, creating a socket repl only splits windows if there is a single group

---
+++ todo.clj
@@ -0,0 +1,25 @@
+(ns todo
+  (:use 
+    arcadia.core 
+    arcadia.linear
+    hard.core))
+
+'[repl
+  ([/] history + navigation
+    ([x] don't record duplicate history)
+    ([ ] prevent editing above history prompt?))]
+
+'[interface
+  ([x] better socket view insertion - only create second group if none exists)
+  ([ ] socket commands in context menu)
+  ([ ] research best default key commands (maybe use sublimeREPL's layout))]
+
+'[advanced
+  ([ ] extend to system processes (pipes))
+  ([ ] allow customizable command wrapping + namespace sniffing
+    ([ ] template with tokens for entered text and regex to apply to current view?))]
+
+'[bugs
+  ]
+
+'(map inc (range 3))
